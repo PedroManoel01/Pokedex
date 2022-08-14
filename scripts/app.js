786b7fb27b87b1694fce3818e0e6e0b3583b23c7
PedroManoel <pmdbma@cin.ufpe.br>
2022-08-13 21:54:58 -0300

    igualando as três telas

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,8 @@
-const menu = document.querySelector('#mobile-menu')
-const menuLinks = document.querySelector('.navbar__menu')
-
-menu.addEventListener('click', function() {
-  
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
-
-})
+for (let i = 1; i <= 150; i++) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    fetch(url)
+      .then(res => res.json())
+      .then(pokemon => {
+        console.log(pokemon);
+      });
+  }
