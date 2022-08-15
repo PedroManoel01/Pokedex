3ec65d51ed7636ce9285c2df2b370425ac9848fe
PedroManoel <pmdbma@cin.ufpe.br>
2022-08-15 12:01:16 -0300

    Added filter pokemon for type

--- scripts/getPokemons.js
+++ scripts/getPokemons.js
@@ -1,25 +1,35 @@
 const poke_container = document.getElementById('poke_container');
-const pokemon_number_1 = 898;
+const poke_cards = document.getElementsByClassName('pokemon');
+var pokemon_number = 898;
 
 const getPokemon = async id => {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
     const res = await fetch(url)
     const pokemon = await res.json();
-    console.log(pokemon.types[0].type.name);
     createPokemonCard(pokemon);
 }
 
-const fetchPokemons = async () => {
-    for(let i=1; i <= pokemon_number_1; i++ ){
+const getPokemonType = async url =>{
+    console.log(url);
+    const res = await fetch(url)
+    const pokemon = await res.json();
+    createPokemonCard(pokemon)
+}
+
+const fetchPokemons = async (pokemon_number) => {
+    for(let i=1; i <= pokemon_number; i++ ){
         await getPokemon(i);
     }
 }
+
+
 
 const createPokemonCard = (pokemon) => {
     const pokemonEl = document.createElement('div');
     pokemonEl.classList.add('pokemon');
     const { id, name, sprites, types } = pokemon;
     const type = types[0].type.name;
+    if(id <= 898){
     const pokeInnerHTML = `
     <div class="img-container">
         <img src="${sprites.front_default}" alt="${name}"/>
@@ -32,6 +42,53 @@ const createPokemonCard = (pokemon) => {
     `;
     pokemonEl.innerHTML = pokeInnerHTML;
     poke_container.appendChild(pokemonEl);
+}}
+
+const getAllTypes = () => {
+    fetch('https://pokeapi.co/api/v2/type')
+        .then(res=> res.json())
+           .then(pokemonType => {
+                pokemonType.results.forEach(function(typeOfPokemon){
+                    console.log(pokemonType)
+                    addButton(typeOfPokemon);
+    })
+    })
+    }
+
+const addButton = (pokemonType) => {
+
+    let select = document.getElementById("botao");
+    let button = document.createElement("button");
+        if(pokemonType.name !== "unknown" & pokemonType.name !== "shadow"){
+        button.innerHTML = `${pokemonType.name}`;
+        select.append(button);
+        }
+        button.onclick = function() {
+            counter = 0
+            for(let i = poke_cards.length - 1; i >= 0; i--){
+                poke_cards[i].remove();
+            }
+            getPokemonByType(pokemonType.url,counter)
+        }
 }
 
-fetchPokemons();
+const getPokemonByType = (typeURL,counter) => {
+    fetch(typeURL)
+        .then(res => res.json())
+           .then(pokemonType => {            
+             pokemonType.pokemon.forEach(function(pokemon){ 
+                getPokemonType(pokemonType.pokemon[counter].pokemon.url)
+                counter = counter +1
+    })
+    })
+    .catch((error) => {
+    console.error("Error:", error);
+    });
+    }
+
+
+
+
+
+fetchPokemons(pokemon_number);
+getAllTypes();
