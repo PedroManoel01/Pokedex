9b97286d695be21e5309482dce0a8218015a4b73
PedroManoel <pmdbma@cin.ufpe.br>
2022-08-14 23:22:45 -0300

    pokemon cards add

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,14 @@
-for (let i = 1; i <= 150; i++) {
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu')
+
+menu.addEventListener('click', function() {
+  
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+
+})
+
+for (let i = 1; i <= 151; i++) {
     const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
     fetch(url)
       .then(res => res.json())
